1de9b028e6918425e362c255345cf84a630c6390
kushal-7 <chhetrikushal14@gmail.com>
2023-11-07 19:36:37 +0600

    Generation_of_image

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
 from flask import Flask,request
 from twilio.twiml.messaging_response import MessagingResponse
 import openai
+
+app = Flask(__name__)
+openai.api_key = "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"
+draw_synonyms = ["draw", "paint", "image", "picture", "painting", "drawing"]
+
+de generate_image(text_prompt):
+    generation_response = openai.Image.create(
+    prompt = text_prompt,
+    n=1,
+    size = "1024x1024",
+    response_format="url",
+    )
+
+    print(generation_response)
+
+    generated_image_url = generation_response["data"][0]["url"]
+    return generated_image_url
